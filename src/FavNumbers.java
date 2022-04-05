bc359b609378a3f2f563286644ba0731ab93874e
Michael McMahon <mcmahom2@mail.uc.edu>
2022-04-04 20:27:26 -0400

    FavNumbers Done

--- src/FavNumbers.java
+++ src/FavNumbers.java
@@ -1,2 +1,73 @@
+import java.util.Scanner;
 public class FavNumbers {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        boolean done = false;
+        int inputInt = 0;
+        double inputDouble = 0;
+        String trash = "";
+
+
+        inputInt = getInt(in, "Please enter your favorite integer: ");
+        inputDouble = getDouble(in, "Please enter your favorite double: ");
+        System.out.println("Your favorite integer is: " + inputInt + " and favorite double is: " + inputDouble);
+    }
+
+    /**
+     *
+     * @param pipe
+     * @param prompt
+     * @return - a number that is an integer
+     */
+    public static int getInt (Scanner pipe, String prompt)
+    {
+        boolean done = false;
+        int inputInt = 0;
+        String trash = "";
+
+        do {
+            System.out.print(prompt);
+            if (pipe.hasNextInt()) {
+                inputInt = pipe.nextInt();
+
+                pipe.nextLine();
+                done = true;
+            } else
+            {
+                trash = pipe.nextLine();
+                System.out.println("Enter a valid integer, not: " + trash);
+            }
+        }while(!done);
+
+        return inputInt;
+    }
+
+    /**
+     *
+     * @param pipe
+     * @param prompt
+     * @return - a number that is a double
+     */
+    public static double getDouble (Scanner pipe, String prompt)
+    {
+        boolean done = false;
+        double inputDouble = 0;
+        String trash = "";
+
+        do {
+            System.out.print(prompt);
+            if (pipe.hasNextDouble()) {
+                inputDouble = pipe.nextDouble();
+                pipe.nextLine();
+                done = true;
+            }else
+            {
+                trash = pipe.nextLine();
+                System.out.println("Enter a valid double not: " + trash);
+            }
+        }while(!done);
+
+        return inputDouble;
+    }
 }
