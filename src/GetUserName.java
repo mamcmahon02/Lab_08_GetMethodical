f62aae0809b717ca8cc44a69090ccd526e6ec089
Michael McMahon <mcmahom2@mail.uc.edu>
2022-04-04 20:22:06 -0400

    Initial commit

---
+++ src/GetUserName.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class GetUserName
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        String firstName = "";
+        String lastName = "";
+
+        firstName = SafeInput.getNonZeroLenString(in, "Enter your first name");
+        lastName = SafeInput.getNonZeroLenString(in, "Enter your last name");
+        System.out.println("\nYour full name is: " + firstName + " " + lastName);
+    }
+
+    public static String getNonZeroLenString(Scanner pipe, String prompt) {
+        String returnString = "";
+        do {
+            System.out.print("\n" + prompt + ": ");
+            returnString = pipe.nextLine();
+        } while (returnString.length() == 0);
+
+        return returnString;
+    }
+}
